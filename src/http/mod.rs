c925f290d6943dc89cf4ec9c6367f785191b6395
cyb3rmavka <pavlenko.maksym.a@gmail.com>
2023-11-26 02:53:00 +0200

    Split into files code

---
+++ src/http/mod.rs
@@ -0,0 +1,4 @@
+pub use request::Request;
+pub use method::Method;
+pub mod method;
+pub mod request;
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,14 @@
-use std::option::Option;
+use server::Server;
+use http::Request;
+use http::Method;
+
+mod http;
+mod server;
 
 fn main() {
 
-    let server = Server::new("127.0.0.1::8082".to_string());
+    let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 
 }
 
-struct Server{
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-
-    fn run(self){
-        todo!();
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: todo!(),
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATH
-}
