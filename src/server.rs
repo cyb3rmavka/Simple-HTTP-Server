94488452937d874e6d8175f901a17fe90761155c
cyb3rmavka <pavlenko.maksym.a@gmail.com>
2023-11-26 04:47:29 +0200

    Implement trait TryFrom for Request

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,7 @@
 use std::io::Read;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use crate::http::Request;
 use std::net::TcpListener;
 pub struct Server {
     addr: String,
@@ -21,6 +24,11 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
                         },
                         Err(e) => println!("Failed to read from connections: {}", e),
                     }
