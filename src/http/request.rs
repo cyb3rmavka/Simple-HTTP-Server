d4dd023c06d39719bfab9e8b54a01195bf3652fc
cyb3rmavka <pavlenko.maksym.a@gmail.com>
2023-11-26 18:26:07 +0200

    create path parser

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,9 +1,10 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult, write};
 use std::str;
 use std::str::Utf8Error;
+use crate::http::method;
 
 pub struct Request {
     path: String,
@@ -18,12 +19,21 @@ impl TryFrom<&[u8]> for Request {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
+
+        let method: Method =  method::parse()?;
+
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i+1..]);
+            path = &path[..i];
+        }
+
         unimplemented!()
     }
 }
@@ -45,6 +55,12 @@ pub enum ParseError {
     InvalidMethod,
 }
 
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
+    }
+}
 impl From<Utf8Error> for ParseError {
     fn from(_: Utf8Error) -> Self {
         Self::InvalidEncoding
