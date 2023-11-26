8dc03e707364d85d07acc3408bcd321725f0de5e
cyb3rmavka <pavlenko.maksym.a@gmail.com>
2023-11-27 00:25:03 +0200

    rewrite handlers

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,12 +1,19 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(
-            StatusCode::Ok,
-            Some("<h1>IT works!!!</h1>".to_string()),
-        )
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
-} 
+}
